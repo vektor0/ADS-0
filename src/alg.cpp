6c72b2d80babeb34874ee728f19547ea7ba7684f
vektor0 <99178713+vektor0@users.noreply.github.com>
2022-02-13 23:53:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
-// Copyright 2021 NNTU-CS
+ @@ -2,4 +2,7 @@
 
-int gcd(int a, int b) { 
-    // поместить сюда текст реализации алгоритма (на С++)
-       if (b == 0)
+ int gcd(int a, int b) { 
+     // поместить сюда текст реализации алгоритма (на С++)
+    if (b == 0)
         return a;
     return gcd(b, a % b);
 }
