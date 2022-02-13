89162ada023f2adf190ccf596792b0f15278d3f2
vektor0 <99178713+vektor0@users.noreply.github.com>
2022-02-14 00:08:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,13 @@
 // Copyright 2021 NNTU-CS
 
+@@ -4,8 +4,7 @@ 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
-}
+    while (a != b) {
+        if (a > b) {
+            a = a-b;
+        }
+        else {
+        } else {
+            b = b-a;
+        }
+    }
